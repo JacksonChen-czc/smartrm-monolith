4fcffdf9206e9a699e5ce4481ecf68bd68bf0a76
chenzc <chenzc@fadada.com>
2022-11-15 14:43:44 +0800

    init

---
+++ src/main/java/cn/chenzecheng/smartrmmonolith/SmartrmMonolithApplication.java
@@ -0,0 +1,13 @@
+package cn.chenzecheng.smartrmmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrmMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrmMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/chenzecheng/smartrmmonolith/SmartrmMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chenzecheng.smartrmmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrmMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
